37715d3d39736beb8121512732577dc1975ec2b8
Vyacheslav Barinov <v.barinov@samsung.com>
2015-02-02 15:55:26 +0300

    [Main] Fixed handling of empty command line
    Minor fix:
        File charset set to utf-8
    
    Change-Id: I4b26ea6606010498a45119eb35ac670df389938a
    Signed-off-by: Vyacheslav Barinov <v.barinov@samsung.com>

--- tsan.py
+++ tsan.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
 import argparse
 import sys
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Creates a firmware with sanitized packages')
+    parser = argparse.ArgumentParser(
+        description='Creates a firmware with sanitized packages')
     args = parser.parse_args()
+    if len(sys.argv) == 1:
+        parser.print_help()
+        exit(0)
     parser.parse_args(sys.argv)
