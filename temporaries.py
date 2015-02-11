e0951976fdceb79bd8c46032c7e09d5b7f61a250
Ilya Palachev <i.palachev@samsung.com>
2015-02-11 13:38:43 +0300

    [Temporaries] rename prefix of temporaries
    From "tizen-sanitizer.*" to "combi-repo.*"
    
    Change-Id: Ia1e7ef5a43cede845b673b5e3ba0c60b46bf8bfb
    Signed-off-by: Ilya Palachev <i.palachev@samsung.com>

--- temporaries.py
+++ temporaries.py
@@ -9,20 +9,20 @@ import logging
 
 # FIXME: Rename default names of temporary files and directories when the
 # default name of our utility will be chosen. Now they all have prefix
-# "tizen-sanitizer". Also it should be better to have some "macro" for it.
+# "combi-repo". Also it should be better to have some "macro" for it.
 
 
 def create_temporary_file(file_suffix):
     """
     Creates temporary file in tmpfs, named as follows:
 
-    /tmp/tizen-sanitizer.<random>.<suffix>
+    /tmp/combi-repo.<random>.<suffix>
 
     @param file_suffix      The suffix of temporary file.
 
     @return                 The path to created temporary file.
     """
-    file_descriptor, path = tempfile.mkstemp(prefix='tizen-sanitizer.',
+    file_descriptor, path = tempfile.mkstemp(prefix='combi-repo.',
                                              suffix="." + file_suffix)
     os.close(file_descriptor)  # This helps to avoid the file descriptor leak.
     atexit.register(os.remove, path)  # It will be removed at exit.
@@ -35,13 +35,13 @@ def create_temporary_directory(directory_suffix):
     """
     Creates temporary directory in tmpfs, named as follows:
 
-    /tmp/tizen-sanitizer.<random>.<suffix>
+    /tmp/combi-repo.<random>.<suffix>
 
     @param file_suffix      The suffix of temporary directory.
 
     @return                 The path to created temporary directory.
     """
-    path = tempfile.mkdtemp(prefix='tizen-sanitizer.',
+    path = tempfile.mkdtemp(prefix='combi-repo.',
                             suffix="." + directory_suffix)
     atexit.register(shutil.rmtree, path)  # It will be removed at exit.
     logging.debug("Created temporary file {0}".format(path))
