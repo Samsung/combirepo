1b812594ea4a7917f3be1a27e1e06784d44fca59
Ilya Palachev <i.palachev@samsung.com>
2015-02-16 18:28:05 +0300

    [Setup] Initial version of setup.py
    It works for the case of run from git directory:
    
    sudo python2.7 ./setup.py build
    sudo python2.7 ./setup.py install
    
    Works also with
    
    sudo pip install -e .
    
    Additional dependencies (Ubuntu naming):
    
    libxml2-dev, zlib1g-dev, libpython-dev, yum, mic
    
    Change-Id: Idd5e997f9a9f500d63ed419a156fc2e3c006282a
    Signed-off-by: Ilya Palachev <i.palachev@samsung.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='combi-repo',
+      version='0.1',
+      description='Image creation tool from COMBInation of REPOsitories',
+      packages=find_packages(),
+      install_requires=['argparse',
+                        'iniparse',
+                        'python-igraph'],
+      requires=['yum', 'mic'])
