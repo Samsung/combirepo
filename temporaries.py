128029ef0186285fd5bba3549a8dde91af5b06b8
Ilya Palachev <i.palachev@samsung.com>
2015-09-03 13:06:09 +0300

    [Temporaries] Added image mount function
    Image will be mounted to the given directory. At the exit of program
    it will be umounted using atexit handler.
    
    Change-Id: I01bc317b66681eb6cef3178997b991632b4cfd0d
    Signed-off-by: Ilya Palachev <i.palachev@samsung.com>

--- temporaries.py
+++ temporaries.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
+import sys
+import subprocess
 import tempfile
 import atexit
 import shutil
@@ -49,3 +51,26 @@ def create_temporary_directory(directory_suffix):
         atexit.register(shutil.rmtree, path)  # It will be removed at exit.
     logging.debug("Created temporary file {0}".format(path))
     return path
+
+
+def mount_image(directory, image_path):
+    """
+    Creates temporary mount point of the given image in the given directory.
+
+    /tmp/combi-repo.<random>.<suffix>
+
+    and mounts the image to it.
+
+    @param directory        The path to the directory.
+    @param image_path       The path to the image to be mounted.
+
+    @return                 The path to created temporary directory.
+    """
+    value = subprocess.call(["mount", "-o", "rw,loop", image_path, directory])
+    if value != 0:
+        logging.error("Failed to mount image.")
+        sys.exit("Error.")
+    if not debug_mode:
+        atexit.register(subprocess.call, ["umount", "-l", directory])
+    logging.debug("Mounted image {0} to {1}".format(image_path, directory))
+    return
