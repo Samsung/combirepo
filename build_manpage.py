8ea87e58c5a47d9cb6a2fd77d352f00f60da73ff
Vyacheslav Barinov <v.barinov@samsung.com>
2016-02-04 15:24:30 +0300

    [All] License information added
    Change-Id: I6912eca95b1cd5a9f5c3d74a7ff8e5c7ca9e15f8
    Signed-off-by: Vyacheslav Barinov <v.barinov@samsung.com>

--- build_manpage.py
+++ build_manpage.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# This file is based on Oz Nahum Tiram <nahumoz@gmail.com> work
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Copyright (C) Samsung Electronics, 2016
+#
+# 2016         Ilya Palachev                 <i.palachev@samsung.com>
+#              Vyacheslav Barinov             <v.barinov@samsung.com>
+# The idea of the module is taken from Oz Nahum Tiram <nahumoz@gmail.com> work
+
 """
 This module builds a standard UNIX manpage from ArgsParse options description
 """
